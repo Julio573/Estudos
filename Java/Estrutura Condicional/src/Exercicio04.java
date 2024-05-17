4fffbc2ba3703afce69594aaaca76d58c3951012
julio cesar <julio8497@hotmail.com>
2024-05-17 14:52:33 -0300

    Adicionando o quarto exercício de estrutura condicional

---
+++ Java/Estrutura Condicional/src/Exercicio04.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Exercicio04 {
+    public static void main(String[] args) {
+        int hora_Inicial, hora_Final, duracao;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Descubra a duração entre dois horários.\n");
+
+        System.out.print("Insira a hora inicial: ");
+        hora_Inicial = scan.nextInt();
+        System.out.print("Insira a hora final: ");
+        hora_Final = scan.nextInt();
+
+        if (hora_Inicial < hora_Final) {
+            duracao = hora_Final - hora_Inicial;
+            System.out.println("A duração é de " + duracao + " horas.");
+        } else {
+            duracao = 24 - hora_Inicial + hora_Final;
+            System.out.println("A duração é de " + duracao + " horas.");
+        }
+        scan.close();
+    }
+}
