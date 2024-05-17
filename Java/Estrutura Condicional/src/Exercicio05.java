2df7bb9f1ed155f3ccaa03d692a4aed37ee99b00
julio cesar <julio8497@hotmail.com>
2024-05-17 16:19:51 -0300

    Adicionando o quinto exercício de estrutura condicional

---
+++ Java/Estrutura Condicional/src/Exercicio05.java
@@ -0,0 +1,32 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicio05 {
+    public static void main(String[] args) {
+        int codigo, quantidade;
+
+        Locale.setDefault(Locale.US);
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Selecione o código do item selecionado e a quantidade\n");
+
+        System.out.print("Selecione o código do item desejado: ");
+        codigo = scan.nextInt();
+        System.out.print("Seleciona a quantidade desejada: ");
+        quantidade = scan.nextInt();
+
+        double total = 0.00;
+        if (codigo == 1) {
+           total = quantidade * 4;
+        } else if (codigo == 2) {
+            total = quantidade * 4.50;
+        } else if (codigo == 3) {
+            total = quantidade * 5.00;
+        } else if (codigo == 4) {
+            total = quantidade * 2.00;
+        } else if (codigo == 5) {
+            total = quantidade * 1.50;
+        }
+
+        System.out.printf("Total: R$%.2f%n", total);
+    }
+}
