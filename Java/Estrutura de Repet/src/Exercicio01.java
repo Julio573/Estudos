ebadac9c63ab1475ca6c360cf84535d7fd3a1f42
julio cesar <julio8497@hotmail.com>
2024-05-20 12:46:12 -0300

    Adicionando primeiro exercicio sobre estruturas de repetição

--- Java/Estrutura de Repet/src/Exercicio01.java
+++ Java/Estrutura de Repet/src/Exercicio01.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Exercicio01 {
+import java.util.Scanner;
+
+public class Exercicio01 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Insira sua senha: ");
+        int senha = scan.nextInt();
+
+        while (senha != 2002) {
+            System.out.println("Senha inválida, tente novamente");
+            System.out.print("Insira sua senha: ");
+            senha = scan.nextInt();
+        }
+
+        System.out.println("Acesso permitido.");
+        scan.close();
+    }
 }
