8f027f94cf76db189d63b771dc9bf0cff8fede87
julio cesar <julio8497@hotmail.com>
2024-05-22 13:13:09 -0300

    Finalizando exercicio 3 que estava pendente

--- Java/Estrutura de Repetição/src/Exercicio03.java
+++ Java/Estrutura de Repetição/src/Exercicio03.java
@@ -7,29 +7,32 @@ public class Exercicio03 {
         Scanner scan = new Scanner(System.in);
 
         codigo = 0;
-        int total = 0;
+        int totalAlcool = 0;
+        int totalGasolina = 0;
+        int totalDiesel = 0;
+
         while (codigo != 4) {
 
             System.out.print("Insira o código do produto desejado: ");
             codigo = scan.nextInt();
 
             if (codigo == 1) {
-                total += 1;
+                totalAlcool += 1;
                 System.out.println("Álcool");
             } else if (codigo == 2) {
-                total += 1;
+                totalGasolina += 1;
                 System.out.println("Gasolina");
             } else if (codigo == 3) {
-                total += 1;
+                totalDiesel += 1;
                 System.out.println("Diesel");
             } else {
                 System.out.println("Insira outro código válido");
             }
         }
-                System.out.println("MUITO OBRIGADO.");
-                System.out.println("Alcool: " + total);
-                System.out.println("Gasolina: " + total);
-                System.out.println("Diesel: " + total);
+        System.out.println("MUITO OBRIGADO.");
+        System.out.println("Alcool: " + totalAlcool);
+        System.out.println("Gasolina: " + totalGasolina);
+        System.out.println("Diesel: " + totalDiesel);
 
         scan.close();
     }
